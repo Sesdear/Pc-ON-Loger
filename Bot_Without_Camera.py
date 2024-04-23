49811122723c277f426feb1ed72a2878038a9dff
HLNikNiky <56186901+Sesdear@users.noreply.github.com>
2024-04-23 17:27:57 +0700

    Create Bot_Without_Camera.py

---
+++ Bot_Without_Camera.py
@@ -0,0 +1,23 @@
+import asyncio
+import datetime
+
+import cv2
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot("Токен Бота")
+chat_id = <Ваш chat_id>
+
+time = datetime.datetime.now()
+
+
+async def start():
+    await bot.send_message(chat_id=chat_id, text=f'Пк запущен: {time}')
+
+
+async def main():
+    await bot.polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(start())
+    exit()
